a8f571b28b16c7a7d34ee42ea28fd5a145823d2c
Ömer ŞAYAK <omersayakk@gmail.com>
2026-02-01 23:17:55 +0300

    feat: configure Next.js to proxy /api requests to the Python backend.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  // Rewrite /api requests to the Python Backend
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:8000/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
